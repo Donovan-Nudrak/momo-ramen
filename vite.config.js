4ff75201a6c84c1c11f4756bae8b0eaa8a918290
Donovan-Nudrak <251686002+Donovan-Nudrak@users.noreply.github.com>
2026-09-05 13:06:02 +0000

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/momo-ramen/',
   plugins: [react(), tailwindcss()],
 })
